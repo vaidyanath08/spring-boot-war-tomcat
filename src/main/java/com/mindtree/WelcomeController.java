16cf116be8dcce4792a2e5e9b6b289dc970f4c4b
Vaidyanath Rajpoot <M1040042@mindtree.com>
2018-10-16 13:15:37 +0530

    update

--- src/main/java/com/mindtree/WelcomeController.java
+++ src/main/java/com/mindtree/WelcomeController.java
@@ -11,7 +11,7 @@ public class WelcomeController {
 
 	// inject via application.properties
 	@Value("${welcome.message:test}")
-	private String message = "Hello World";
+	private String message = "welcome to springboot ..Hello World";
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
