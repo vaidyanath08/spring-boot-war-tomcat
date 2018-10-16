a7403c2e139d707677d8969b99111bd848f3f706
Vaidyanath Rajpoot <M1040042@mindtree.com>
2018-10-16 12:58:41 +0530

    update

---
+++ src/main/java/com/mindtree/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.mindtree;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return message;
+	}
+
+}
