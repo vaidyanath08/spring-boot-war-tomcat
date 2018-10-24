3a029f696926409aba5454ac7e37e912beaf480d
Vaidyanath Rajpoot <M1040042@mindtree.com>
2018-10-24 19:12:55 +0530

    update

---
+++ terraform/vars.tf
@@ -0,0 +1,15 @@
+
+{
+variable "AWS_ACCESS_KEY" {
+
+}
+variable "AWS_SECRET_KEY" {
+
+}
+variable "AWS_REGION" {
+default = "us-east-2"
+}
+
+"base_instance_type" : "t2.micro",
+	"ami_tag_type_value" : "base-ami-packer"
+	}
